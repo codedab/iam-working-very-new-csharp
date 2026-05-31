6b926e136337636b179416e6d2b5e760e0785e16
haybeing-glitch <haybeing@gmail.com>
2026-05-31 23:43:35 +0100

    fix: remove UseUrls override to let ASPNETCORE_URLS bind correctly

--- Program.cs
+++ Program.cs
@@ -2,10 +2,6 @@ using IdentityApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Read port from ASPNETCORE_URLS env var; default to 8080
-var urls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://+:8080";
-builder.WebHost.UseUrls(urls);
-
 builder.Services.AddSingleton<UserStore>();
 builder.Services.AddSingleton<TokenService>();
 builder.Services.AddSingleton<RoleStore>();
